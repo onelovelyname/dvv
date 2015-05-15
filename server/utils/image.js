9b0a89cc94b16590626a6dea384445dd86c88400
Michael Harris <mwharris333@gmail.com>
2015-05-14 20:04:09 -0700

    add comments for image handling methods

--- server/utils/image.js
+++ server/utils/image.js
@@ -30,19 +30,31 @@ imageUtils.createDataArrayFromJpegFile = function(filename, callback){
 
     if (err) return console.log(err);
 
+    // take jpeg data (from file read) and decode it into an object
+    // {width: ... , height: ... , data: <buffer ... >}
     var parsedData = jpeg.decode(data);
 
-    var dataArray = [];
+    // create a dataArray to hold the pixels from the raw image data
+    var dataArray = []; // dataArray = [ [r,g,b,a], [r,g,b,a], [r,g,b,a], [r,g,b,a], ... ]
     for (var i=0; i<parsedData.data.length; i+=4) {
+
+      // grab the rgba values (red, green, blue, alpha) from the raw image data
       var r = parsedData.data[i];
       var g = parsedData.data[i+1];
       var b = parsedData.data[i+2];
       var a = parsedData.data[i+3];
-      var rgba = [r,g,b,a];
+      var rgba = [r,g,b,a]; // individual pixel
 
+      // push the pixel to the dataArray
       dataArray.push(rgba);
     }
 
+    // callback that makes use of the dataArray
+    // the callback needs to hold onto the width and height.
+    // dataArray is a flat array, has no way to express width and height,
+    // which will be needed by the createJpegFileFromDataArray function
+    // to reassemble the jpeg from the incoming dataArray formed from the
+    // data coming from the clients.
     callback(dataArray, parsedData.width, parsedData.height);
 
   });
@@ -53,6 +65,12 @@ imageUtils.createDataArrayFromJpegFile = function(filename, callback){
 
 imageUtils.createJpegFileFromDataArray = function(filename, dataArray, width, height, callback){
 
+  // dataArray is assumed to be the same format as what was produced from createDataArrayFromJpegFile(...)
+  // [ [r,g,b], [r,g,b], [r,g,b], [r,g,b], ... ] , except with the alpha missing
+
+  // the jpeg image needs to have the alpha value even though jpegs don't use transparency.
+  // rgbToRgbaDataBuffer takes the rgb triplet from dataArray along with a default alpha value
+  // to populate the raw image data buffer for the jpeg file. 
   var rgbToRgbaDataBuffer = new Buffer(width * height * 4);
   for (var i=0; i<dataArray.length; i++) {
     rgbToRgbaDataBuffer[i*4] = dataArray[i][0];   // Red
@@ -61,14 +79,17 @@ imageUtils.createJpegFileFromDataArray = function(filename, dataArray, width, he
     rgbToRgbaDataBuffer[i*4+3] = 255;             // Alpha
   }
 
+  // raw image data object needed by the jpeg encode function
   var rawImageData = {
     data: rgbToRgbaDataBuffer,
     width: width,
     height: height
   };
 
+  // jpeg image data needed to write the file
   var jpegImageData = jpeg.encode(rawImageData, 100);
 
+  // write jpeg file, then execute a callback on completion
   fs.writeFile(filename, jpegImageData.data, function(err) {
     if(err) return console.log(err);
     callback();
